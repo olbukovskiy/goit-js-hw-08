96844fdd4230be88cb5d57bdff5211d0ff2e64a6
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-10-08 13:47:39 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryVoid = document.querySelector('.gallery');
+
+function markupCreator(galleryItems) {
+  return galleryItems.map(
+    item => `
+    <a class='gallery__item' href='${item.original}'>
+    <img class='gallery__image' src='${item.preview}' alt='${item.description}' />
+  </a>`
+  ).join(" ");
+}
+const newMarkup = markupCreator(galleryItems);
+
+galleryVoid.innerHTML = newMarkup;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
