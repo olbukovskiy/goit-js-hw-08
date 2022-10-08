50ddb456f1f28cb53e998aa664d4e746846730d9
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-10-08 22:08:54 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,20 @@
+import vimeoPlayer from '@vimeo/player';
 
-import vimeoPlayer from "@vimeo/player"
-
-const iframeVideo = document.querySelector("#vimeo-player");
+const iframeVideo = document.querySelector('#vimeo-player');
 const player = new vimeoPlayer(iframeVideo);
-console.log(player)
+const throttler = require('lodash.throttle');
+
+player.on('timeupdate', throttler(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', Math.round(data.seconds));
+}
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    console.log('Current player time: ', seconds);
+  })
+  .catch(function (error) {
+    console.log('Attention mate, we get an' + error.name + 'error here!');
+  });
