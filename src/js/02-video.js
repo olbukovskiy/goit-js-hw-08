b8225f3be1270084d83546a45836c02638965f8b
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-10-09 18:19:03 +0300

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,7 @@
 import vimeoPlayer from '@vimeo/player';
 
 const iframeVideo = document.querySelector('#vimeo-player');
+
 const player = new vimeoPlayer(iframeVideo);
 const throttler = require('lodash.throttle');
 
@@ -10,11 +11,4 @@ function onPlay(data) {
   localStorage.setItem('videoplayer-current-time', Math.round(data.seconds));
 }
 
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {
-    console.log('Current player time: ', seconds);
-  })
-  .catch(function (error) {
-    console.log('Attention mate, we get an' + error.name + 'error here!');
-  });
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,61 @@
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  emailField: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
 
+const throttler = require('lodash.throttle');
+const STORAGE_KEY = 'feedback-form-state';
+let values = {};
+
+refs.emailField.addEventListener(
+  'input',
+  throttler(feedbacEmailkDataHandler, 500)
+);
+
+refs.textarea.addEventListener(
+  'input',
+  throttler(feedbackTextareaDataHandler, 500)
+);
+
+function feedbacEmailkDataHandler(event) {
+  values.email = event.target.value;
+  let storageValues = JSON.stringify(values);
+  localStorage.setItem(STORAGE_KEY, storageValues);
+}
+
+function feedbackTextareaDataHandler(event) {
+  values.message = event.target.value;
+  let storageValues = JSON.stringify(values);
+  localStorage.setItem(STORAGE_KEY, storageValues);
+}
+
+window.addEventListener('load', getStorageDataHandler);
+
+function getStorageDataHandler() {
+  let storageData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (storageData?.email) {
+    refs.emailField.value = storageData.email;
+  }
+
+  if (storageData?.message) {
+    refs.textarea.value = storageData.message;
+  }
+}
+
+refs.feedbackForm.addEventListener('submit', formResetHandler);
+
+function formResetHandler(event) {
+  event.preventDefault();
+
+  let storageData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (storageData) {
+    console.log(storageData);
+  }
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  values = {};
+}
